dc6ed9dd00bacf31e9e773fc67ae7d334e20ca30
dysthn <1690968562@qq.com>
2021-04-08 20:02:01 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
+import axios from "axios";
+Vue.prototype.$axios = axios;
 new Vue({
-    router,
-    store,
-    render: (h) => h(App),
+  router,
+  store,
+  render: (h) => h(App),
 }).$mount("#app");
